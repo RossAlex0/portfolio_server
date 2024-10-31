7d862ce04db7e22e0eacf9e9c5dfa5d09409f90c
Alex Rossignol <alexrossignol@macbookair.home>
2024-10-31 13:08:26 +0100

    Updated middleware

--- src/middleware/transportEmail.js
+++ src/middleware/transportEmail.js
@@ -4,38 +4,38 @@ const { EMAIL_USER, EMAIL_PASSWORD } = process.env;
 
 const createEmail = async (req, res, next) => {
   try {
-    next();
-    //     const { name, email, subject, message } = req.body;
-    //     console.info("create", req.body);
-    //     const transporter = nodemailer.createTransport({
-    //       service: "Gmail",
-    //       auth: {
-    //         user: EMAIL_USER,
-    //         pass: EMAIL_PASSWORD,
-    //       },
-    //     });
+    const { name, email, subject, message } = req.body;
+    console.info("create", req.body);
+    const transporter = nodemailer.createTransport({
+      service: "Gmail",
+      auth: {
+        user: EMAIL_USER,
+        pass: EMAIL_PASSWORD,
+      },
+    });
 
-    //     const mailOptions = {
-    //       from: EMAIL_USER,
-    //       to: EMAIL_USER,
-    //       subject: `From portfolio - Nouveau message de ${name} : ${email}`,
-    //       text: `
-    //        Nom: ${name}    ||    Email: ${email}
+    const mailOptions = {
+      from: EMAIL_USER,
+      to: EMAIL_USER,
+      subject: `From portfolio - Nouveau message de ${name} : ${email}`,
+      text: `           
+       Nom: ${name}    ||    Email: ${email}
 
-    // ----------------------------------------------------
+----------------------------------------------------
 
-    //     Sujet : ${subject}
+    Sujet : ${subject} 
+      
+      
+${message}`,
+    };
 
-    // ${message}`,
-    //     };
+    const response = await transporter.sendMail(mailOptions);
 
-    //     const response = await transporter.sendMail(mailOptions);
-
-    //     if (response) {
-    //       next();
-    //     } else {
-    //       res.status(500).json({ message: "Erreur lors de l'envoi de l'email." });
-    //     }
+    if (response) {
+      next();
+    } else {
+      res.status(500).json({ message: "Erreur lors de l'envoi de l'email." });
+    }
   } catch (error) {
     res.json({ error: error, message: "transport" });
   }
