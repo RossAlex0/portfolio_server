a3e243405f7ab1444fe4595cec3abfc3b3343f76
Rossignol Alex <alexnw33910@gmail.com>
2025-08-30 22:59:19 +0200

    refactor: update api response to return correct wine fields

--- src/controller/excel/index.ts
+++ src/controller/excel/index.ts
@@ -5,7 +5,7 @@ import { Request, Response } from "express";
 const EXCEL_PATH = "utils/doc_tarif_07082025.xlsx";
 
 export const parseExcel = async (_req: Request, res: Response) => {
-  const excelFilePath = path.join(process.cwd(), EXCEL_PATH); // process.cwd() = racine projet
+  const excelFilePath = path.join(process.cwd(), EXCEL_PATH);
 
   const workbook = new ExcelJS.Workbook();
   const worksheet = (await workbook.xlsx.readFile(excelFilePath)).getWorksheet(
@@ -13,22 +13,31 @@ export const parseExcel = async (_req: Request, res: Response) => {
   );
 
   if (worksheet) {
-    const parsed: Record<string, string>[] = [];
+    const parsed: Record<string, string | Record<string, string>>[] = [];
 
     worksheet.eachRow((row, rowNumber) => {
       if (rowNumber === 1) return;
+      const rawPack = row.getCell(3).text ?? "";
+
+      const packType = rawPack.replace(/\d/g, "") ?? null;
+
+      const packCount = rawPack.replace(/\D/g, "") ?? null;
+
       parsed.push({
-        un: row.getCell(1).text ?? null,
-        deux: row.getCell(2).text ?? null,
-        trois: row.getCell(3).text ?? null,
-        quatre: row.getCell(4).text ?? null,
-        cinq: row.getCell(5).text ?? null,
-        six: row.getCell(6).text ?? null,
+        name: row.getCell(1).text ?? null,
+        year: row.getCell(2).text ?? null,
+        pack_bottle: {
+          type: packType,
+          count: packCount,
+        },
+        volume: row.getCell(4).text ?? null,
+        stock: row.getCell(5).text ?? null,
+        price: row.getCell(6).text ?? null,
       });
     });
 
     res.status(200).json(parsed);
   } else {
-    res.status(500).json({ error: "Can't found the excel file" });
+    res.status(500);
   }
 };
